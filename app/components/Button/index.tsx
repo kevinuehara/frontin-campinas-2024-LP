20558340581b8a4240617f0a82b340c9a0ee6d26
Paulo Henrique Vieira Candido <pcandido@cpqd.com.br>
2024-02-26 13:56:40 -0300

    Bloqueia botão de call4Papers temporariamente

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -8,22 +8,26 @@ interface ButtonProps {
 
 export const Button = ({ className, ...props }: ButtonProps) => {
   return (
-    <a
-      href="https://forms.gle/5gXM1uCewkgmH1Uq9"
-      target="_blank"
-      className={clsx(
-        `font-semibold rounded text-lg p-[0.94rem] ${className}`,
-        css({
-          backgroundColor: colors.primary,
-          transition: "filter 0.2s",
-          "&:hover": {
-            filter: "brightness(0.9)",
-          },
-        })
-      )}
-      {...props}
-    >
-      Se inscreva pelo Call4Papers
-    </a>
+    <>
+      {/* <a
+        aria-disabled="true"
+        href="#"
+        target="_blank"
+        className={clsx(
+          `font-semibold rounded text-lg p-[0.94rem] ${className}`,
+          css({
+            backgroundColor: colors.primary,
+            transition: "filter 0.2s",
+            "&:hover": {
+              filter: "brightness(0.9)",
+            },
+          })
+        )}
+        {...props}
+      >
+        Se inscreva pelo Call4Papers
+      </a> */}
+      <p className="p-[0.94rem] mt-5">Em breve Call4Papers...</p>
+    </>
   );
 };
