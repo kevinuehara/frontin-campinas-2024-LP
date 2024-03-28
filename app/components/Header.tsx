8a7638b5d002ac0a47bdf007c8cc2c8b1874b1b5
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2024-03-27 21:53:38 -0300

    Ajusta posicionamento do logo no header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { Button, Container } from ".";
 import { DateIcon, LocationIcon } from "../assets";
+import { css } from "@emotion/css";
 
 /**
  * Header da página Home
@@ -18,37 +19,55 @@ export const Header = () => {
           height={1400}
           priority
         />
-        <div className=" flex flex-col items-center content-center gap-[1rem]">
-          <div className="flex align-items-center justify-items-center gap-[1rem]">
-            <DateIcon />
-            <label className="text-2xl font-extrabold">
-              25 de Maio de 2024
-            </label>
-          </div>
-          <div className="flex align-items-center justify-items-center gap-[1rem]">
-            <LocationIcon />
-            <label className="text-lg font-medium ">
-              Pólis de Alta Tecnologia
-            </label>
-          </div>
-        </div>
+
         <section className="h-screen z-2">
           <div className="flex flex-col items-center justify-center h-[80%]">
-            <div data-aos="fade-up" className="p-4 md:p-0">
-              <Image
-                src="/frontin-home.svg"
-                alt="FrontIn Campinas Logo"
-                width={600}
-                height={600}
-                priority
-              />
-            </div>
+            <div className="flex gap-[41px]">
+              <div>
+                <Image
+                  src="/static/logo-color.png"
+                  alt="FrontIn Campinas Logo"
+                  width={217}
+                  height={600}
+                  className="top-[-57px] relative"
+                  priority
+                />
+              </div>
+              <div className="flex flex-col gap-[2.2rem]">
+                <div data-aos="fade-up" className="p-4 md:p-0">
+                  <Image
+                    src="/static/logo-base.png"
+                    alt="FrontIn Campinas Logo"
+                    width={386}
+                    height={600}
+                    className={css({})}
+                    priority
+                  />
+                </div>
+                <div className="flex flex-col gap-[1rem]">
+                  <div className="flex align-items-center justify-items-center gap-[1rem]">
+                    <DateIcon />
+                    <label className="text-2xl font-extrabold">
+                      25 de Maio de 2024
+                    </label>
+                  </div>
+                  <div className="flex align-items-center justify-items-center gap-[1rem]">
+                    <LocationIcon />
+                    <label className="text-lg font-medium ">
+                      Pólis de Alta Tecnologia
+                    </label>
+                  </div>
+                </div>
 
-            <Button
-              data-aos-delay="400"
-              data-aos="fade-up"
-              className="mt-6 z-2"
-            />
+                <div>
+                  <Button
+                    data-aos-delay="400"
+                    data-aos="fade-up"
+                    className="mt-6 z-2"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </Container>
